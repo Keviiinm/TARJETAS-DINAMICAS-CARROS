28055a08c857730fabaaf06ea8b0689692a6be94
Kevin Montanez <kevindanilo281@gmail.com>
2025-09-26 13:37:46 -0500

    añadi funcion de agregar carros al panel y cambie variables porque me daba error

--- js/main.js
+++ js/main.js
@@ -1,207 +1,188 @@
-// *CREAMOS LAS CONSTANTES PARA LOS ELEMNTOS PRINCIPALES
-const inputFoto = document.getElementById("foto")
-const inputNombre = document.getElementById("nombre")
-const inputMarca = document.getElementById("marca")
-const inputModelo = document.getElementById("modelo")
-const inputKilometraje = document.getElementById("kilometraje")
-const inputPrecio = document.getElementById("precio")
-const card = document.getElementById("cont-cards2")
-const addBtn = document.getElementById("agregar")
-
-// *CREAMOS LA FUNCION QUE NOS PERMITA CREAR UNA NUEVA TAREA APARTIR DEL FORMULARIO
-// *TODA ETIQUETA QUE VAMOS ACREAR ES APARTIR DE LA MAQUETA HTML PRE EXISTENTE
-
-function createVehicle(foto, nombre, marca, modelo, kilometraje, precio) {
-
-    // *creamos el nodo o elemento padre
-    const padre = document.createElement("div");
-    padre.classList.add("col-md-6", "cont-cards");
+const inputFoto = document.getElementById('foto');
+const inputNombre = document.getElementById('nombre');
+const inputMarca = document.getElementById('marca');
+const inputModelo = document.getElementById('modelo');
+const inputKilometraje = document.getElementById('kilometraje');
+const inputPrecio = document.getElementById('precio');
+const cards = document.getElementById('cont-cardss');
+const addBtn = document.getElementById('agregar')
+const panelLateral = document.querySelector('.panel')
 
 
-    const col = document.createElement("div");
-    col.classList.add("card", "h-100");
+// creamos la funcion que nos permite crear una nueva tarea a partir del formulario
+// toda etiqueta que vamos a crear es partir de la maqueta html preexistente
 
-    const img = document.createElement("img");
-    img.src = foto
-    img.alt = "foto vehiculo"
-    img.classList.add("card-img-top", "w-100");
+// esta funcion solo crea la estructura del html y la deja en un limbo, aun no la inserta en la pagina
 
 
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
+function createVehicle(foto,nombre,marca,modelo,kilometraje,precio){
 
-    const h3 = document.createElement("h3");
-    h3.classList.add("card-title");
-    h3.textContent = nombre;
+    // cramos el nodo o elemento padre
 
-    const h4Marca = document.createElement("h4");
-    h4Marca.classList.add("h4");
-    h4Marca.textContent = marca;
+    // contenedor padre de los item
+    const divPadre = document.createElement('div');
+        divPadre.classList.add('col-md-6' , 'item-vehiculo');
 
-    const h4Modelo = document.createElement("h4");
-    h4Modelo.classList.add("card-text");
-    h4Modelo.textContent = ("Modelo: ") + modelo;
+    const altura = document.createElement('div');
+        altura.classList.add('card' , 'h-100');
 
-    const h4km = document.createElement("h4");
-    h4km.classList.add("card-text");
-    h4km.textContent = ("kilometraje: ") + kilometraje;
+    const imagen = document.createElement('img');
+        imagen.classList.add('card-img-top' , 'w-100');
+        imagen.setAttribute('src' , foto)
 
-    const precio1 = document.createElement("h2");
-    precio1.classList.add("text-success");
-    precio1.textContent = ("$ ") + precio;
+    const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+    const nombreCarro = document.createElement('h3');
+        nombreCarro.classList.add('card-title');
+        nombreCarro.textContent=nombre
+
+    const marcaCarro = document.createElement('h4');
+        marcaCarro.classList.add('card-subtitle' , 'text-muted');
+        marcaCarro.textContent=marca
+
+    const modeloCarro = document.createElement('h4');
+        modeloCarro.classList.add('card-text');
+        modeloCarro.textContent='modelo: ' + modelo
+
+    const kilometrajeCarro = document.createElement('h4');
+        kilometrajeCarro.classList.add('card-text')   
+        kilometrajeCarro.textContent='km: ' + kilometraje
+
+    const precioCarro = document.createElement('h2');
+        precioCarro.classList.add('text-success');
+        precioCarro.textContent='$: ' + precio
+
+    const divBotones = document.createElement('div');
+        divBotones.classList.add('d-flex' , 'justify-content-between' , 'mt-3');
 
 
-    const divBtns = document.createElement("div");
-    divBtns.classList.add("d-flex", "justify-content.between", "mt-3");
+    const botonAgregar = document.createElement('button');
+        botonAgregar.classList.add('btn', 'btn-success');
+        botonAgregar.textContent = 'Seleccionar';
 
-    const btnComprar = document.createElement("button");
-    btnComprar.classList.add("btn", "btn-success");
-    btnComprar.id = 'button_comprar';
-    btnComprar.textContent = "Comprar";
+    const botonEliminar = document.createElement('button');
+        botonEliminar.classList.add('btn' , 'btn-danger');
+        botonEliminar.textContent='eliminar';
 
-    const btnEliminar = document.createElement("button");
-    btnEliminar.classList.add("btn", "btn-danger");
-    btnEliminar.textContent = "Eliminar";
 
-    // *ENSAMBLAMOS LA ESTRUCTURA
-     padre.appendChild(col);
-     col.appendChild(img);
-    col.appendChild(cardBody);
+    // ensamblamos dentro del nodo padre sus nodos hijos, es decir la estructura de la card
 
-    
+    divPadre.appendChild(altura)
+    altura.appendChild(imagen)
+    altura.appendChild(cardBody)
+    cardBody.appendChild(nombreCarro)
+    cardBody.appendChild(marcaCarro)
+    cardBody.appendChild(modeloCarro)
+    cardBody.appendChild(kilometrajeCarro)
+    cardBody.appendChild(precioCarro)
+    cardBody.appendChild(divBotones)
+    divBotones.appendChild(botonAgregar)
+    divBotones.appendChild(botonEliminar)
 
-    cardBody.appendChild(h3);
-    cardBody.appendChild(h4Marca);
-    cardBody.appendChild(h4Modelo);
-    cardBody.appendChild(h4km);
-    cardBody.appendChild(precio1);
-    cardBody.appendChild(divBtns);
-    divBtns.appendChild(btnComprar);
-    divBtns.appendChild(btnEliminar);
 
-    
+    // utilizamos el return para retornar o dar respuesta del elemento creado ya que lo usaremos en otra funcion
 
-   
-
-    // retornar la tarjeta completa
-    return padre;
+    return divPadre;
 }
 
 
-const form = document.getElementById("vehiculo-form");
+// detectamos el click o evento click sobre el boton agregar con un evento de escucha o listener para que a partir de este evento se agregue la tarea dentro del contenedor cont-cards
 
+const form = document.getElementById('vehiculo-form')
+form.addEventListener('submit' , (e)=>{
 
-form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    // captura los valores de los inputs
-    const foto = inputFoto.value.trim();
+    let foto = inputFoto.value.trim();
     const nombre = inputNombre.value.trim();
     const marca = inputMarca.value.trim();
     const modelo = inputModelo.value.trim();
     const kilometraje = inputKilometraje.value.trim();
     const precio = inputPrecio.value.trim();
 
-    // crea la tarjeta
-    const newCard = createVehicle(foto, nombre, marca, modelo, kilometraje, precio);
-    card.appendChild(newCard)
-    // agrega al contenedor
-
-
-    if (foto == "") {
+    if (foto=="") {
         foto = 'https://cdn-icons-png.freepik.com/512/6356/6356630.png'
     }
 
-    if (foto == "" || nombre == "" || marca == "" || modelo == "" || kilometraje == "" || precio == "") {
-        alert("no se puede crear una tarea vacia")
-    } else {
-        const newCol = createVehicle(foto, nombre, marca, modelo, kilometraje, precio);
-        // col.appendChild(newCol);
-
-        // inputFoto.value = "";
-        // inputNombre.value = "";
-        // inputMarca.value = "";
-        // inputModelo.value = "";
-        // inputKilometraje.value = "";
-        // inputPrecio.value = "";
-form.reset();
-eventsToVehicles(newCol)
-
+    if (foto=="" || nombre=="" || marca=="" || modelo=="" || kilometraje=="" || precio=="") {
+        alert("no se puede crear un item vacio");
+    }else{
+        const newVehicle = createVehicle(foto,nombre,marca,modelo,kilometraje,precio);
+        eventsToVehicles(newVehicle);
+        cards.appendChild(newVehicle);  
+        inputFoto.value="";
+        inputNombre.value="";
+        inputMarca.value="";
+        inputModelo.value="";
+        inputKilometraje.value="";
+        inputPrecio.value="";
     }
-    // resetear formulario
-    
-
-
-
 });
 
 
-
-// !CARRITO
-
-function eventsToVehicles(padre) {
+function eventsToVehicles(padre){
 
     // utilizamos querySelector para capturar el  input y el button que estan dentro del item
-    const botonAgregar = padre.querySelector('#button_comprar');
+    const botonAgregar = padre.querySelector('.btn-success');
     const botonEliminar = padre.querySelector('.btn-danger');
 
 
     // cuando le de en agregar
-    botonAgregar.addEventListener('click', () => {
-        const fotoPanel = padre.querySelector('img').getAttribute('src');
-        const nombrePanel = padre.querySelector('.card-title').textContent;
-        const marcaPanel = padre.querySelector('.card-subtitle', 'text-muted').textContent;
-        const precioPanel = padre.querySelector('.text-success').textContent;
+    botonAgregar.addEventListener('click' , ()=>{
+       const fotoPanel = padre.querySelector('img').getAttribute('src');
+       const nombrePanel = padre.querySelector('.card-title').textContent;
+       const marcaPanel = padre.querySelector('.card-subtitle' , 'text-muted').textContent;
+       const precioPanel = padre.querySelector('.text-success').textContent;
 
-        const newPanel = itemPanel(fotoPanel, nombrePanel, marcaPanel, precioPanel)
+       const newPanel = itemPanel(fotoPanel,nombrePanel,marcaPanel,precioPanel)
 
-        document.querySelector('.cont-products').appendChild(newPanel);
+       document.querySelector('.panel').appendChild(newPanel);
     });
 
     // evento de eliminar de la de vehiculos
-    botonEliminar.addEventListener('click', () => {
+    botonEliminar.addEventListener('click', ()=>{
         padre.remove();
     });
-    
 }
 
 
 
 const panel = document.querySelector('.panel');
-const carrito = document.getElementById('carrito-panel');
+const carrtio = document.getElementById('carrito');
 
-carrito.addEventListener('click', ()=> {
-    panel.classList.toggle('activo');
-});
+carrtio.addEventListener('click' , ()=>{
+    panel.classList.toggle('activo')
+})
 
 
-function itemPanel(foto, nombre, marca, precio) {
+function itemPanel(foto, nombre, marca, precio){
 
     const divPadrePanel = document.createElement('div');
-    divPadrePanel.classList.add('row', 'tarjeta');
+        divPadrePanel.classList.add('row' , 'tarjeta');
 
     const contImgPanel = document.createElement('div');
-    contImgPanel.classList.add('col-md-4', 'cont-img');
+        contImgPanel.classList.add('col-md-4' , 'cont-img');
 
     const imgPanel = document.createElement('img');
-    imgPanel.setAttribute('src', foto);
+        imgPanel.setAttribute('src' , foto);
 
     const contInfoPanel = document.createElement('div');
-    contInfoPanel.classList.add('col-md-8', 'cont-info');
+        contInfoPanel.classList.add('col-md-8' , 'cont-info');
 
     const h3Nombre = document.createElement('h3');
-    h3Nombre.textContent = nombre
+        h3Nombre.textContent = nombre
 
     const h3Marca = document.createElement('h3');
-    h3Marca.textContent = marca;
+        h3Marca.textContent = marca;
 
     const h3Precio = document.createElement('h3');
-    h3Precio.textContent = precio
+        h3Precio.textContent = precio
 
     const btnEliminar = document.createElement('h3');
-    btnEliminar.classList.add('btn-original')
-    btnEliminar.textContent = 'X'
-    btnEliminar.addEventListener('click', () => divPadrePanel.remove());
+        btnEliminar.classList.add('btn-original')
+        btnEliminar.textContent = 'X'
+        btnEliminar.addEventListener('click', () => divPadrePanel.remove());
 
     // ENSAMBLAMOS TODO EL HTML DEL ITEM DE LA CARD DEL PANEL
 
